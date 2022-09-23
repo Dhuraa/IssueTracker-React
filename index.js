ada4179ca1fc28c7fcea2137808965beb4e94731
Dhura Mistry <dhuramistry@gmail.com>
2022-09-23 18:57:31 -0400

    23sep lec

---
+++ index.js
@@ -0,0 +1,17 @@
+const express = require("express")
+
+const app = express();
+
+const port = '3000';
+app.use(express.static('./public'))
+
+app.get('/', function(req, res){
+    res.render('index.html')
+})
+
+app.listen(port, function(){
+    console.log("WebServer is running");
+})
+
+//npm run compile
+//npm install --save-dev @babel/cli @babel/core @babel/preset-react
